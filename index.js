04dc56c1c33834af99b80be27fc83a067f9c31cc
Tom Wesselman <twesselman@gmail.com>
2014-09-28 15:36:11 +0000

    changed to mongoose

--- index.js
+++ index.js
@@ -4,8 +4,59 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://twesselman:mongouser@ds039850.mongolab.com:39850/heroku_app30126243';
+
+/*
+var mongo = require('mongodb');
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  console.log("Connected to " + mongoUri)
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+      console.log("Wrote key")
+    });
+  });
+});
+*/
+
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
+
+db.on('error', console.error);
+db.once('open', function() {
+  // Create your schemas and models here.
+});
+
+mongoose.connect(mongoUri);
+
+var movieSchema = new mongoose.Schema({
+  title: { type: String }
+, rating: String
+, releaseYear: Number
+, hasCreditCookie: Boolean
+});
+
+// Compile a 'Movie' model using the movieSchema as the structure.
+// Mongoose also creates a MongoDB collection called 'Movies' for these documents.
+var Movie = mongoose.model('Movie', movieSchema);
+
+var thor = new Movie({
+  title: 'Thor'
+, rating: 'PG-13'
+, releaseYear: '2011'  // Notice the use of a String rather than a Number - Mongoose will automatically convert this for us.
+, hasCreditCookie: true
+});
+
+thor.save(function(err, thor) {
+  if (err) return console.error(err);
+  console.dir(thor);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello with Mongoose!')
 })
 
 app.listen(app.get('port'), function() {
