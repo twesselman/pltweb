6fb3c5977bde2709acd2d359f7b5882364dc2212
Tom Wesselman <twesselman@gmail.com>
2014-09-28 19:49:50 +0000

    added swagger

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-var express = require('express')
-var app = express();
+var express = require("express")
+    , swagger = require("swagger-node-express")
+    , path = require('path')
+    , argv = require('minimist')(process.argv.slice(2))
+    , test = require("./models/test")
+    , models = require("./models/models")
+    , app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/lib', express.static(path.join(__dirname, 'swagger-ui/lib')));
+app.use('/css', express.static(path.join(__dirname, 'swagger-ui/css')));
+app.use('/images', express.static(path.join(__dirname, 'swagger-ui/images')));
+
+// Couple the application to the Swagger module.
+swagger.setAppHandler(app);
+
+// Adding models and methods to our RESTFul service
+swagger.addModels(models)
+    .addGet(test.dummyTestMethod);
+ 
+// set api info
+swagger.setApiInfo({
+    title: "Plantronics Management API",
+    description: "API to manage Plantronics devices",
+    termsOfServiceUrl: "",
+    contact: "tom.wesselman@plantronics.com",
+    license: "",
+    licenseUrl: ""
+});
+
 
 var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
@@ -32,33 +60,62 @@ db.once('open', function() {
 
 mongoose.connect(mongoUri);
 
-var movieSchema = new mongoose.Schema({
-  title: { type: String }
-, rating: String
-, releaseYear: Number
-, hasCreditCookie: Boolean
+var userSchema = new mongoose.Schema({
+  firstName: { type: String }
+, lastName: {type: String }
+, email: String
+, employee_id: Number
+, is_active: Boolean
 });
 
-// Compile a 'Movie' model using the movieSchema as the structure.
-// Mongoose also creates a MongoDB collection called 'Movies' for these documents.
-var Movie = mongoose.model('Movie', movieSchema);
+// Compile a 'User' model using the userSchema as the structure.
+// Mongoose also creates a MongoDB collection called 'Users' for these documents.
+var User = mongoose.model('User', userSchema);
 
-var thor = new Movie({
-  title: 'Thor'
-, rating: 'PG-13'
-, releaseYear: '2011'  // Notice the use of a String rather than a Number - Mongoose will automatically convert this for us.
-, hasCreditCookie: true
+var tom = new User({
+  firstName: 'Tom'
+, lastName: 'Wesselman'
+, email: 'twesselman@gmail.com'
+, employee_id: '12345'  // Notice the use of a String rather than a Number - Mongoose will automatically convert this for us.
+, is_active: true
 });
 
-thor.save(function(err, thor) {
+tom.save(function(err, tom) {
   if (err) return console.error(err);
-  console.dir(thor);
+  console.dir(tom);
 });
 
 app.get('/', function(request, response) {
-  response.send('Hello with Mongoose!')
+//    response.send('nothing');
+    response.sendfile(__dirname + '/swagger-ui/index.html')
 })
+
+// Set api-doc path
+swagger.configureSwaggerPaths('', 'api-docs', '');
+
+// Configure the API domain
+var domain = argv.domain;
+var port = app.get('port')
+
+console.log('Domain: ' + domain);
+
+/// is that required???????
+
+// Set and display the application URL
+var applicationUrl = 'http://' + domain + ':' + port;
+
+console.log('snapJob API running on ' + applicationUrl);
+ 
+//swagger.configure(applicationUrl, '1.0.0');
+
+//*** fix this
+
+swagger.configure('https://pltweb-c9-twesselman.c9.io', '1.0.0');
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
